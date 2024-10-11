8ecfdbd98beb0b079a7986112cd9bca32a6b3d1c
praneeth531641 <praneethrayavarapu@gmail.com>
2024-10-11 23:35:43 +0530

    patch4

--- src/components/SocialNetworks/SocialNetworks.jsx
+++ src/components/SocialNetworks/SocialNetworks.jsx
@@ -9,13 +9,13 @@ const SocialNetworks = () => {
       <h1 className="social-title">Welcome to My Social Networks</h1>
       <p className="social-description">These are the Social Networking Accounts I have:</p>
       <div className="social-icons">
-        <a href="https://www.instagram.com/yourusername" target="_blank" rel="noopener noreferrer" className="social-icon">
+        <a href="https://www.instagram.com/_a_r_i_s_e_?utm_source=qr&igsh=MW1vOG5zcmlqdTQ3bw==" target="_blank" rel="noopener noreferrer" className="social-icon">
           <FontAwesomeIcon icon={faInstagram} size="2x" />
         </a>
-        <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer" className="social-icon">
+        <a href="https://github.com/praneeth531641" target="_blank" rel="noopener noreferrer" className="social-icon">
           <FontAwesomeIcon icon={faGithub} size="2x" />
         </a>
-        <a href="https://www.linkedin.com/in/yourusername" target="_blank" rel="noopener noreferrer" className="social-icon">
+        <a href="https://www.linkedin.com/in/praneeth-rayavarapu-8602811a3/" target="_blank" rel="noopener noreferrer" className="social-icon">
           <FontAwesomeIcon icon={faLinkedin} size="2x" />
         </a>
         {/* <a href="https://www.naukri.com/mnjuser/profile" target="_blank" rel="noopener noreferrer" className="social-icon">
